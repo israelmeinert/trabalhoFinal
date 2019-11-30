61e31d30f888ad9a67f085ee031b5834bf58a606
israel <israelmeinert@gmail.com>
2019-11-30 02:58:35 -0300

    added env to recive mongo url

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const Temperature = require('./model/temperture');
 const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
-
-mongoose.connect("mongodb://localhost:27017/final_homework");
-
-
-
 const PORT = (process.ENV && process.ENV.PORT) || 3000;
+const URL_MONGO = (process.ENV && process.ENV.MONGO_URL) || "mongodb://localhost:27017/";
+
+
+mongoose.connect(URL_MONGO, 
+{ useNewUrlParser: true, useUnifiedTopology: true, retryWrites: true, dbName: 'final_homework' });
+
 
 app.get('/temperature', async (req, res) => {
     try {
